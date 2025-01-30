e4527ac84db6239b089abb314d9ef4d28b71446a
ldlazaro2 <ldlazaro2@student.apc.edu.ph>
2025-01-30 08:12:45 +0800

    added personal profile component

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,9 +2,10 @@
 
 <template>
   <h1>Food</h1>
-  <food-item/>
-  <food-item2/>
-  <food-item/>
+    <personal-profile/> <br>
+  <food-item/> <br>
+  <food-item2/> <br>
+  <food-item/> <br>
 </template>
 
 <style></style>
---
+++ vue-project/src/components/PersonalProfile.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <title>Personal Profile Web Page</title>
+    <h1>Personal Profile Web Page</h1>
+
+    <h2>About Me</h2>
+    <p>I am Luis Lorenzo D. Lazaro.</p>
+    <p>I am currently enrolled at Asia Pacific College in the course BSCS-SF231.</p>
+
+    <h3>Education / Achievements</h3>
+    <p>I have graduated senior high from Marikina Science High School.</p>
+
+    <h3>IT Experience</h3>
+    <ul>
+      <li>Contributed to a working website and app for a school project here in APC, the website and application was named Rams Pawtners.</li>
+      <li>Completed a course in Code Combat.</li>
+    </ul>
+
+    <h3>Hobbies and Goals</h3>
+    <table border="1" cellpadding="10" cellspacing="0">
+      <thead>
+        <tr>
+          <th>Hobbies & Interests</th>
+          <th>Goals in Life / Dream</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Playing basketball</td>
+          <td>To finish college</td>
+        </tr>
+        <tr>
+          <td>Playing video games</td>
+          <td>To give back to my parents for their sacrifices</td>
+        </tr>
+        <tr>
+          <td>Listening to music</td>
+          <td>To be a successful professional in my chosen field</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script></script>
+
+<style></style>
